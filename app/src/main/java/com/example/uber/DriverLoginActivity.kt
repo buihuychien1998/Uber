dcfe67ae2e54820fd575550f803ab23c450a5abf
buihuychien1998 <35371090+buihuychien1998@users.noreply.github.com>
2020-08-02 19:19:37 +0700

    first commit

---
+++ app/src/main/java/com/example/uber/DriverLoginActivity.kt
@@ -0,0 +1,67 @@
+package com.example.uber
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_driver_login.*
+
+class DriverLoginActivity : AppCompatActivity(), View.OnClickListener {
+    private var firebaseAuth: FirebaseAuth? = null
+    private var firebaseAuthStateListener: FirebaseAuth.AuthStateListener? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_driver_login)
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseAuthStateListener = FirebaseAuth.AuthStateListener {
+            val user = FirebaseAuth.getInstance().currentUser
+            user?.let {
+                startActivity(Intent(this, DriverMapActivity::class.java))
+                finish()
+            }
+        }
+    }
+
+    override fun onClick(p0: View?) {
+        val email = edtEmail.text.toString()
+        val password = edtPassword.text.toString()
+        when(p0?.id){
+            R.id.btnLogin->{
+               firebaseAuth?.signInWithEmailAndPassword(email, password)?.addOnCompleteListener {
+                   if (!it.isSuccessful){
+                       Toast.makeText(this, "Sign In Error", Toast.LENGTH_SHORT).show()
+                   }
+               }
+
+            }
+            R.id.btnRegistration->{
+                firebaseAuth?.createUserWithEmailAndPassword(email, password)?.addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val uid = firebaseAuth?.uid
+                        val docData = hashMapOf(
+                            uid to true)
+                        val db = FirebaseFirestore.getInstance()
+                        db.collection("Users").document("Drivers").set(docData)
+
+                    }else{
+                        Toast.makeText(this, "Sign Up Error", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        firebaseAuth?.addAuthStateListener { firebaseAuthStateListener }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        firebaseAuth?.removeAuthStateListener { firebaseAuthStateListener }
+    }
+}
---
+++ app/src/main/java/com/example/uber/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.uber
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0?.id){
+            R.id.btnDriver->{
+                startActivity(Intent(this, DriverLoginActivity::class.java))
+                finish()
+            }
+            R.id.btnCustomer->{
+                startActivity(Intent(this, CustomerLoginActivity::class.java))
+                finish()
+            }
+
+        }
+    }
+}
